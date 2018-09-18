50126dd65db85f99851f864bf5273aecc312d4b3
Andy Mender <andymenderunix@gmail.com>
2018-09-18 23:00:02 +0200

    Expand main server service

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from flask import Flask
+import os
+
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 
+from libs.bloglib import EntryFactory
 from libs.sqllib import create_tables, get_posts
 
-app = Flask(__name__)
+# load .env file
+load_dotenv(find_dotenv())
 
-SQLDB = 'mender.db'
+# set up Flask application
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='public',
+            template_folder='templates')
+
+SQL_DB = os.environ.get('SQL_DB')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    engine = create_engine(f'sqlite:///{SQLDB}')
+    engine = create_engine(f'sqlite:///{SQL_DB}')
 
     create_tables(engine)
 
-    # app.run()
+    app.run(os.environ.get('FLASK_HOST'),
+            os.environ.get('FLASK_PORT'),
+            debug=True)
