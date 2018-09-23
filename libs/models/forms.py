6c6616248b56c859fe976a2be86f7330ef9255f5
Andy Mender <andymenderunix@gmail.com>
2018-09-23 15:19:09 +0200

    Add comment Web form to validate comments

---
+++ libs/models/forms.py
@@ -0,0 +1,8 @@
+from wtforms import Form, StringField, TextAreaField, validators
+
+
+class CommentForm(Form):
+    """Blog entry comment form"""
+    name = StringField('name', [validators.required(), validators.length(max=20)])
+    email = StringField('email', [validators.optional(), validators.length(min=5, max=50)])
+    content = TextAreaField('content', [validators.required(), validators.length(min=5, max=1000)])
