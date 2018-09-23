79d921be0684331c9bba980c7530dc806105d114
Andy Mender <andymenderunix@gmail.com>
2018-09-23 15:17:42 +0200

    Split factory classes and models according to MVC-like paradigm

---
+++ libs/models/factories.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+from sqlalchemy.engine import Engine
+
+from libs.controllers import get_comments, get_posts
+from libs.models.models import Comment, Entry
+
+
+class CommentFactory:
+    """Factory class to generate comments"""
+    @staticmethod
+    def from_sql(engine: Engine, post_id: int, comment_id: int) -> Optional[Comment]:
+        """Generate a Comment object from a SQL table
+
+        :param engine: SQLAlchemy engine object
+        :param post_id: Entry identifier in comments table
+        :param comment_id: Comment identifier in comments table
+        :return: populated Comment object
+        """
+        comments = get_comments(engine, post_id, comment_id)
+
+        comment = None
+
+        # populate comment if 'id' exists
+        if len(comments) > 0:
+            c = comments[0]
+
+            comment = Comment(c['post_id'],
+                              c['name'],
+                              c['content'],
+                              c['email'])
+
+        return comment
+
+
+class EntryFactory:
+    """Factory class to generate entries"""
+    @staticmethod
+    def from_sql(engine: Engine, id: int) -> Optional[Entry]:
+        """Generate an Entry object from a SQL table
+
+        :param engine: SQLAlchemy engine object
+        :param id: Entry identifier in table
+        :return: populated Entry object
+        """
+        posts = get_posts(engine, id)
+
+        entry = None
+
+        # populate entry if 'id' exists
+        if len(posts) > 0:
+            post = posts[0]
+
+            entry = Entry(post['id'],
+                          post['title'],
+                          post['filename'],
+                          post['tags'])
+
+        return entry
---
+++ libs/models/models.py
@@ -0,0 +1,71 @@
+import logging
+
+from sqlalchemy.engine import Engine
+from sqlalchemy.sql import text
+
+
+class Comment:
+    """Blog entry comment object representation"""
+    def __init__(self, post_id: int, name: str, content: str, email: str = None):
+        self.post_id = post_id
+        self.name = name
+        self.content = content
+        self.email = email
+
+    def get_logger(self):
+        """Create logger object for the class"""
+        return logging.getLogger(self.__class__.__name__)
+
+    def to_sql(self, engine: Engine) -> bool:
+        """Send comment to database table via provided engine
+
+        :param engine: SQLAlchemy engine object
+        :return: True on success, False on failure
+        """
+        sql_query = text('INSERT OR REPLACE INTO comments'
+                         ' (name, email, post_id, content)'
+                         ' VALUES (:name, :email, :post_id, :content)')
+
+        data = {'name': self.name,
+                'email': self.email,
+                'post_id': self.post_id,
+                'content': self.content}
+
+        with engine.connect() as conn:
+            conn.execute(sql_query, **data)
+
+        return True
+
+
+class Entry:
+    """Blog entry object representation"""
+    def __init__(self, post_id: int, title: str, filename: str, tags: list = None):
+        self.post_id = post_id
+        self.title = title
+        self.filename = filename
+        self.tags = tags if tags is not None else []
+        self.logger = self.get_logger()
+
+    def get_logger(self):
+        """Create logger object for the class"""
+        return logging.getLogger(self.__class__.__name__)
+
+    def to_sql(self, engine: Engine) -> bool:
+        """Send entry to database table via provided engine
+
+        :param engine: SQLAlchemy engine object
+        :return: True on success, False on failure
+        """
+        sql_query = text('INSERT OR REPLACE INTO entries'
+                         ' (id, title, filename, tags)'
+                         ' VALUES (:id, :title, :filename, :tags)')
+
+        data = {'id': self.post_id,
+                'title': self.title,
+                'filename': self.filename,
+                'tags': ','.join(self.tags)}
+
+        with engine.connect() as conn:
+            conn.execute(sql_query, **data)
+
+        return True
