946facc90c44400ef215c15139c5c56b120bab48
Andy Mender <andymenderunix@gmail.com>
2025-12-10 10:49:03 +0100

    Update form handling to latest version of WTForms

--- models/forms.py
+++ models/forms.py
@@ -3,7 +3,7 @@ from wtforms import Form, StringField, TextAreaField, validators
 
 class CommentForm(Form):
     """Blog entry comment form"""
-    name = StringField('name', [validators.required(), validators.length(max=20)])
+    name = StringField('name', [validators.data_required(), validators.length(max=20)])
     email = StringField('email', [validators.optional(), validators.length(min=5, max=50)])
-    content = TextAreaField('content', [validators.required(), validators.length(min=5, max=1000)])
+    content = TextAreaField('content', [validators.data_required(), validators.length(min=5, max=1000)])
     occupation = StringField('occupation', [validators.optional(), validators.length(max=100)])
